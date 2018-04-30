6aca67b4bd6b2f12757edd33e77ace8ec1697986
Amin007 Ledang <helmi007@yahoo.com>
2018-04-30 17:02:33 +0800

    buat cari untuk id dan nama syarikat

---
+++ Aplikasi/Kelas/Utama/Kawal/cari.php
@@ -0,0 +1,119 @@
+<?php
+namespace Aplikasi\Kawal; //echo __NAMESPACE__; 
+class Cari extends \Aplikasi\Kitab\Kawal
+{
+#==========================================================================================
+	function __construct()
+	{
+		parent::__construct();
+		//\Aplikasi\Kitab\Kebenaran::kawalMasuk();
+		\Aplikasi\Kitab\Kebenaran::kawalKeluar();
+		$this->_folder = huruf('kecil', namaClass($this));
+	}
+
+	public function index()
+	{
+		# Set pemboleubah utama
+		$this->papar->tajuk = namaClass($this);
+		//echo '<hr> Nama class : ' . namaClass($this) . '<hr>';
+
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->paparKandungan($this->_folder, 'index', $noInclude=0);
+	}
+
+	public function paparKandungan($folder, $fail, $noInclude)
+	{	# Pergi papar kandungan
+		$jenis = $this->papar->pilihTemplate($template=0);
+		$this->papar->bacaTemplate(
+		//$this->papar->paparTemplate(
+			$this->_folder . '/' . $fail, $jenis, $noInclude); # $noInclude=0
+			//'mobile/mobile',$jenis,0); # $noInclude=0
+		//*/
+	}
+
+	public function semakPembolehubah($senarai)
+	{
+		echo '<pre>$senarai:<br>';
+		print_r($senarai);
+		echo '</pre>|';//*/
+	}
+
+	function logout()
+	{
+		//echo '<pre>sebelum:'; print_r($_SESSION) . '</pre>';
+		\Aplikasi\Kitab\Sesi::destroy();
+		header('location: ' . URL);
+		//exit;
+	}
+#==========================================================================================
+#-------------------------------------------------------------------------------------------
+	public function suku1($action = 'hasil')
+	{
+		# Set pemboleubah utama
+		$this->papar->tajuk = namaClass($this);
+		//echo '<hr> Nama class : ' . namaClass($this) . '<hr>';
+
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->paparKandungan($this->_folder, $pilihFail, $noInclude=1);
+	}
+#-------------------------------------------------------------------------------------------
+	public function pembolehubah()
+	{
+        //echo '<pre>$_POST=>'; print_r($_POST) . '</pre>';
+        /*  $_POST[] => Array ( [cari] => 0000000123456 or [nama] => ABC ) */
+
+        $myJadual = array('aes','kawalan_aes','aes_alam_sekitar',
+		'aes_kp_205','aes_kp_206','aes_kp_207','aes_kp_800',
+		'aes_perkhidmatan','aes_pertanian');
+		$medan = '*';
+        $this->papar->senarai = array();
+        # cari id berasaskan newss/ssm/sidap/nama
+        $id['nama'] = bersih(isset($_POST['cari']) ? $_POST['cari'] : null);
+        //$id['nama'] = isset($_POST['id']['nama']) ? $_POST['id']['nama'] : null;
+		$bilSemua = $item = 10; $ms = 1; ## set pembolehubah utama
+		//echo '$bilSemua:' . $bilSemua . ', $item:' . $item . ', $ms:' . $ms . '<br>';
+		$jum = pencamSqlLimit($bilSemua, $item, $ms);
+		$susun[] = array_merge($jum, array('kumpul'=>null,'susun'=>'nama') );
+
+		return array($myJadual, $medan, $id, $this->papar->senarai, $susun); //*/
+	}
+#-------------------------------------------------------------------------------------------
+	public function idnama() 
+	{	//echo '<br>Anda berada di class Cari extends Kawal:idnama()<br>';       
+        # senaraikan tatasusunan jadual
+		list($myJadual, $medan, $id, $this->papar->senarai, $susun) = $this->pembolehubah();
+        if (!empty($id['nama'])) 
+        {
+			$carian[] = array('fix'=>'z%like%', # cari = atau %%
+				'atau'=>'WHERE', # WHERE / OR / AND
+				'medan' => 'concat_ws("",newss,nossm,nama)', # cari dalam medan apa
+				'apa' => $id['nama']); # benda yang dicari
+			
+            foreach ($myJadual as $key => $myTable)
+            {# mula cari $cariID dalam $myJadual
+                $this->papar->senarai[$myTable] = 
+					$this->tanya->cariSemuaData("`$myTable`", $medan, $carian, $susun);
+					//$this->tanya->cariSql("`$myTable`", $medan, $carian, $susun);
+            }# tamat
+
+			# isytihar pembolehubah untuk dalam class Papar
+			$this->papar->primaryKey = 'newss';
+			$this->papar->carian[] = $id['nama'];
+        }
+        else
+        {
+            $this->papar->carian[]='[id:0]';
+        }
+			
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->paparKandungan($this->_folder, 'a_syarikat' , $noInclude=0); 
+		//*/
+    }
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#==========================================================================================
+}
