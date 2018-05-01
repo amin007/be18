91672a91a2d9fe5171c96d510044bc078ea3b213
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-01 18:28:31 +0800

    buat fail biodata

---
+++ Aplikasi/Kelas/Utama/Kawal/biodata.php
@@ -0,0 +1,79 @@
+<?php
+namespace Aplikasi\Kawal; //echo __NAMESPACE__; 
+class Cari extends \Aplikasi\Kitab\Kawal
+{
+#===========================================================================================
+	function __construct()
+	{
+		parent::__construct();
+		//\Aplikasi\Kitab\Kebenaran::kawalMasuk();
+		\Aplikasi\Kitab\Kebenaran::kawalKeluar();
+		$this->_folder = huruf('kecil', namaClass($this));
+	}
+
+	public function index()
+	{
+		# Set pemboleubah utama
+		$this->papar->tajuk = namaClass($this);
+		//echo '<hr> Nama class : ' . namaClass($this) . '<hr>';
+
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->paparKandungan($this->_folder, 'index', $noInclude=0);
+	}
+
+	public function paparKandungan($folder, $fail, $noInclude)
+	{	# Pergi papar kandungan
+		$jenis = $this->papar->pilihTemplate($template=0);
+		$this->papar->bacaTemplate(
+		//$this->papar->paparTemplate(
+			$this->_folder . '/' . $fail, $jenis, $noInclude); # $noInclude=0
+			//'mobile/mobile',$jenis,0); # $noInclude=0
+		//*/
+	}
+
+	public function semakPembolehubah($senarai)
+	{
+		echo '<pre>$senarai:<br>';
+		print_r($senarai);
+		echo '</pre>|';//*/
+	}
+
+	function logout()
+	{
+		//echo '<pre>sebelum:'; print_r($_SESSION) . '</pre>';
+		\Aplikasi\Kitab\Sesi::destroy();
+		header('location: ' . URL);
+		//exit;
+	}
+#===========================================================================================
+#-------------------------------------------------------------------------------------------
+	public function contoh($action = 'hasil')
+	{
+		# Set pemboleubah utama
+		$this->papar->tajuk = namaClass($this);
+		//echo '<hr> Nama class : ' . namaClass($this) . '<hr>';
+
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->paparKandungan($this->_folder, $pilihFail, $noInclude=1);
+	}
+#-------------------------------------------------------------------------------------------
+	public function pembolehubah()
+	{
+	}
+#-------------------------------------------------------------------------------------------
+	public function register() 
+	{		
+		# Set pemboleubah utama
+
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai); # Semak data dulu
+		$this->paparKandungan($this->_folder, 'a_syarikat' , $noInclude=0); 
+		//*/
+    }
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------
+#===========================================================================================
+}
