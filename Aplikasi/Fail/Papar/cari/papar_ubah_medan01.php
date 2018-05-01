67eafca6e8c508e0310a37f1aa028d8082f5de50
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-01 22:48:22 +0800

    tukar nama medan kepada medan01. tambah html->updateInput()

--- Aplikasi/Fail/Papar/cari/papar_ubah_medan01.php
+++ Aplikasi/Fail/Papar/cari/papar_ubah_medan01.php
@@ -3,16 +3,15 @@
 	for ($kira=0; $kira < count($row); $kira++)
 	{## papar data $row ------------------------------------------------
 	?><tbody><?php
-		//$html = new \Aplikasi\Kitab\Html_TD;
+		$html = new \Aplikasi\Kitab\Html_Input;
 		foreach ( $row[$kira] as $key=>$data )
 		{
-			?><tr><?php
-			?><td><?php echo $key ?></td><?php
-			?><td><?php echo $data ?></td><?php
-			?></tr><?php
+			?><tr><td><?php echo $key ?></td><?php
+			$paparData = $html->updateInput($myTable, $kira, $key, $data)
+			?><td><?php echo $paparData . "\n\t"
+			?></td></tr><?php
 		} 
 		?></tbody>
 	<?php
 	}#-----------------------------------------------------------------
-	?>
-	</table>
+	?></table>
