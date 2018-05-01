39ab8ed9f075d1ebeb1e33f70db1cfd427aa2f72
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-02 01:25:25 +0800

    tambah $ aksi dan $ html->medanHantar

--- Aplikasi/Fail/Papar/cari/papar_ubah_medan02.php
+++ Aplikasi/Fail/Papar/cari/papar_ubah_medan02.php
@@ -1,23 +1,25 @@
-<form class="form-horizontal"><!-- form class="form-inline" -->
 <?php
 $html = new \Aplikasi\Kitab\Html_Input;
-$html->medanTajuk($myTable, $class = 'col-sm-7');
-$class = 'col-sm-6 '; # untuk $data
+$aksi = URL . 'biodata/ubahSimpan';
+$class1 = 'col-sm-7'; # untuk tajuk dan hantar
+$class2 = 'col-sm-6 '; # untuk $data ?>
+<form form method="POST" action="<?php echo $aksi ?>"
+class="form-horizontal"><!-- class="form-inline" --><?php
+$html->medanTajuk($myTable, $class1);
 for ($kira=0; $kira < count($row); $kira++)
-{## papar data $row ------------------------------------------------
-	foreach ( $row[$kira] as $key=>$data )
-	{
+{	foreach ( $row[$kira] as $key=>$data )
+	{## papar data $row ------------------------------------------------
 		?><div class="form-group"><?php echo "\n\t";
 		?><label for="inputTajuk" class="col-sm-1 control-label"><?php echo $key 
 		?></label><?php //echo "\n\t"; 
-		/*?><div class="<?php echo $class ?>"><?php*/
-		$paparData = $html->updateInput($class, $myTable, $kira, $key, $data);
+		/*?><div class="<?php echo $class2 ?>"><?php*/
+		$paparData = $html->updateInput($class2, $myTable, $kira, $key, $data);
 		echo $paparData . "\n";
 		/*?></div><!-- / class="<?php echo $class ?>" --><?php*/
 		?></div><!-- / class="form-group" --><?php
 		echo "\n";
-	} 
+	}#-----------------------------------------------------------------
 }#-----------------------------------------------------------------
-//include 'z_medan02_hantar.php'; ?>
+$html->medanHantar($myTable, $class1); ?>
 </form><!-- / class="form-horizontal" -->
 <!-- / class="container" -->
