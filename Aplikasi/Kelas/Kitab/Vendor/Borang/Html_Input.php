a7e3ff99cd9a36204271b8e46e3f51c04510e630
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-02 00:37:10 +0800

    tambah fungsi labelPre, classInput, komen

--- Aplikasi/Kelas/Kitab/Vendor/Borang/Html_Input.php
+++ Aplikasi/Kelas/Kitab/Vendor/Borang/Html_Input.php
@@ -30,14 +30,31 @@ class Html_Input
 	public static function labelBawah($labelDibawah)
 	{
 		$input2 = null;
+		$tab2 = "\n\t\t";
 		$input2 = ($labelDibawah==null) ? '' : 
 				'<span class="input-group-addon">' 
-				. $labelDibawah . '</span>';
+				. $labelDibawah . '</span>'
+				. $tab2;
 
 		return $input2;
 	}
 #------------------------------------------------------------------------------------------
-	public function updateInput($myTable, $kira, $namaMedan, $data)
+	public static function labelPre($labelDibawah)
+	{
+		$input2 = null;
+		$tab2 = "\n\t\t";
+		$pre = 'pre';
+		//$pre = 'blockquote';
+		$input2 = ($labelDibawah==null) ? '' : 
+				'<' . $pre . '>' 
+				. $labelDibawah 
+				. '</' . $pre . '>'
+				. $tab2;
+
+		return $input2;
+	}
+#------------------------------------------------------------------------------------------
+	public function updateInput($class, $myTable, $kira, $namaMedan, $data)
 	{
 		# istihar pembolehubah 
 		$medan = dpt_senarai('jadual_biodata2');
@@ -55,44 +72,47 @@ class Html_Input
 		$birutua = 'btn btn-primary btn-mini';
 		$birumuda = 'btn btn-info btn-mini';
 		$merah = 'btn btn-danger btn-mini';
+		$classInput = $class . 'input-group input-group';
+		$komen = '<!-- / "' . $class . 'input-group input-group" -->';
 
 		if(in_array($jenisMedan,array('textbox')))
 		{#kod utk input text 
-			$input = $tab2 . '<div class="input-group input-group">' . $tab3
+			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
 				   //. '<span class="input-group-addon"></span>' . $tab3
 				   . '<input type="text" ' . $name . ' class="form-control">' . $tab3
 				   . $this->labelBawah($labelDibawah)
-				   . $tab2 . '</div>'
+				   . '</div>' . $komen
 				   . '';
 		}
 		elseif(in_array($namaMedan,array('website_id', 'item_id', 'category_id', 'rating_id')))
 		{#untuk medan primary key
 			$data = null;
-			$input = $tab2 . '<div class="input-group input-group">' . $tab3
+			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
 				   //. '<span class="input-group-addon"></span>' . $tab3
 				   . $this->labelBawah($labelDibawah)
-				   . $tab2 . '</div>'
+				   . '</div>' . $komen
+				   . '';
+		}
+		elseif(in_array($namaMedan,array('CatatNota')))
+		{#senarai medan untuk textarea
+			//$data = null;
+			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
+				   . '<textarea ' . $name . ' rows="5" cols="20"' . $tab3
+				   . ' class="form-control">' . $data . '</textarea>' . $tab3 
+				   . $this->labelPre($labelDibawah)
+				   . '</div>' . $komen . $tab3 
 				   . '';
 		}
 		elseif(in_array($namaMedan,$medan))
 		{#senarai medan untuk table admin_website
 			//$data = null;
-			$input = $tab2 . '<div class="input-group input-group">' . $tab3
+			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
 				   //. '<span class="input-group-addon"></span>' . $tab3
 				   . '<input type="text" ' . $name 
 				   . ' value="' . $data . '"'
 				   . ' class="form-control">' . $tab3
 				   . $this->labelBawah($labelDibawah)
-				   . $tab2 . '</div>'
-				   . '';
-		}
-		elseif(in_array($namaMedan,array('key_googleapi', 'cse_googleapi')))
-		{#senarai medan untuk textarea
-			//$data = null;
-			$input = $tab2 . '<div class="input-group input-group">' . $tab3
-				   . '<textarea ' . $name . ' rows="5" cols="20"' . $tab3
-				   . ' class="form-control">' . $data . '</textarea>' . $tab3 
-				   . '</div>' . $this->labelBawah($labelDibawah)
+				   . '</div>' . $komen
 				   . '';
 		}
 		
