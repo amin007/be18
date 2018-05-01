3e8e47cca17a188d612eea3882d271e1ef351b15
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-02 00:35:20 +0800

    tukar dari ubahmedan kepada ubah_medan02

--- Aplikasi/Fail/Papar/cari/b_ubah.php
+++ Aplikasi/Fail/Papar/cari/b_ubah.php
@@ -3,5 +3,5 @@
 //include 'papar_jadual_berulang.php';
 //$pilihJadual = 'jadual_operasi';
 //$pilihJadual = 'jadual_am';
-$pilihJadual = 'ubah_medan';
+$pilihJadual = 'ubah_medan02';
 include 'papar_jadual_berulang_mudah.php';
