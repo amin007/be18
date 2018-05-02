4924eef7e7efe6f684b326ca1de190cf9d560e43
Amin007 Ledang <helmi007@yahoo.com>
2018-05-02 08:39:04 +0800

    1. tambah $carian = '| carian : ' . $this->carian[0]; 2. masukkan jadual_

--- Aplikasi/Fail/Papar/cari/a_syarikat.php
+++ Aplikasi/Fail/Papar/cari/a_syarikat.php
@@ -1,6 +1,9 @@
 <?php
 
+//echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
+//echo '<pre>$carian='; print_r($this->carian[0]); echo '</pre>';
+$carian = '| carian : ' . $this->carian[0];
 //include 'papar_jadual_berulang.php';
-$pilihJadual = 'operasi';
-//$pilihJadual = 'am';
+$pilihJadual = 'jadual_operasi';
+//$pilihJadual = 'jadual_am';
 include 'papar_jadual_berulang_mudah.php';
