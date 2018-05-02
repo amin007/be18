981d055c94c7ce667cad8b91f47fb37839c7816b
Amin007 Ledang <helmi007@yahoo.com>
2018-05-02 18:11:52 +0800

    tambah $carian = '| jumlah : ' . $this->cariID;

--- Aplikasi/Fail/Papar/cari/a_syarikat.php
+++ Aplikasi/Fail/Papar/cari/a_syarikat.php
@@ -2,7 +2,8 @@
 
 //echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
 //echo '<pre>$carian='; print_r($this->carian[0]); echo '</pre>';
-$carian = '| carian : ' . $this->carian[0];
+$carian = '| jumlah : ' . $this->cariID;
+//$carian = '| carian : ' . $this->carian[0];
 //include 'papar_jadual_berulang.php';
 $pilihJadual = 'jadual_operasi';
 //$pilihJadual = 'jadual_am';
