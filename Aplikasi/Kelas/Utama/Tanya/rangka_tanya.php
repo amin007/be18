3f9dad51290960c4c39c2a4e3fbafa10fc7df51a
Amin007 Ledang <helmi007@yahoo.com>
2018-05-02 18:09:53 +0800

    tambah class Rangka_Tanya

---
+++ Aplikasi/Kelas/Utama/Tanya/rangka_tanya.php
@@ -0,0 +1,73 @@
+<?php
+namespace Aplikasi\Tanya; //echo __NAMESPACE__; 
+class Rangka_Tanya extends \Aplikasi\Kitab\Tanya
+{
+#=====================================================================================================
+	public function __construct()
+	{
+		parent::__construct();
+	}
+#---------------------------------------------------------------------------------------------------#
+	function data_contoh($pilih)
+	{
+		$data = array(
+			'namaPendek' => 'james007',
+			'namaPenuh' => 'Polan Bin Polan',
+			'level' => 'pelawat'
+		); # dapatkan medan terlibat
+		$kira = 1; # kira jumlah data
+		
+		return ($pilih==1) ? $kira : $data; # pulangkan nilai
+	}
+#---------------------------------------------------------------------------------------------------#
+	function alihMedan()
+	{
+		//ALTER TABLE Employees MODIFY COLUMN empName VARCHAR(50) AFTER department;
+	}
+#---------------------------------------------------------------------------------------------------#
+	public function semakPosmen($myTable, $posmen, $pass)
+	{
+		if(isset($posmen[$myTable][$pass])):
+			if($posmen[$myTable][$pass] == null):
+				//echo '<br> buang ' . $pass;
+				unset($posmen[$myTable][$pass]);
+			else:
+				$posmen[$myTable][$pass] = 
+					\Aplikasi\Kitab\RahsiaHash::rahsia('md5', 
+					$posmen[$myTable][$pass]);
+			endif;
+		endif;
+
+		return $posmen;
+	}
+#---------------------------------------------------------------------------------------------------#
+	public function medanKawalan($cariID) 
+	{ 
+		$news1 = 'http://sidapmuar/ekonomi/ckawalan/ubah/' . $cariID;
+		$news2 = 'http://sidapmuar/ekonomi/cprosesan/ubah/000/'.$cariID.'/2010/2015/'; 
+		$news3 = 'http://sidapmuar/ekonomi/semakan/ubah/",kp,"/'.$cariID.'/2010/2015/'; 
+		$url1 = '" <a target=_blank href=' . $news1 . '>SEMAK 1</a>"' . "\r";
+		$url2 = '" <a target=_blank href=' . $news2 . '>SEMAK 2</a>"' . "\r";
+		$url3 = 'concat("<a target=_blank href=' . $news3 . '>SEMAK 3</a>")' . "\r";
+        $medanKawalan = '`newss`,`bandar`,'
+			. 'concat_ws("|",nama,operator) nama,'
+			. '`kod_daerah`,`daerah`,`po`,'
+			. '`survei`,kp,msic2008,' 
+			//. 'concat_ws("-",kp,msic2008) msic2008,' . "\r"
+			//. 'concat_ws("-",kp,msic2008) keterangan,' . "\r"
+			. ' concat_ws("|",' . "\r"
+			. ' 	concat_ws("="," responden",responden),' . "\r"
+			. ' 	concat_ws("="," notel",notel),' . "\r"
+			. ' 	concat_ws("="," nofax",nofax),' . "\r"
+			. ' 	concat_ws("="," orang_",orang_a),' . "\r"
+			. ' 	concat_ws("="," notel_a",notel_a),' . "\r"
+			. ' 	concat_ws("="," nofax_a",nofax_a)' . "\r"
+ 			. ' ) as dataHubungi,' . "\r"
+			. 'concat_ws(" ",alamat1,alamat2,poskod,bandar) as alamat' . "\r"
+			. '';	
+		return $medanKawalan;
+	}
+#---------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------------#
+#=====================================================================================================
+}
