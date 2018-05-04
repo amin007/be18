d11a744cdd7bc8de1f1fa8d3c8ddb0f939fbbc6c
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-04 09:03:50 +0800

    buat fail baru

---
+++ Aplikasi/Fail/Papar/cari/index.php
@@ -0,0 +1,16 @@
+<?php
+# untuk carian syarikat sahaja
+/*echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
+echo '<pre>$carian='; print_r($this->carian[0]); echo '</pre>';
+echo '<pre>$cariID='; print_r($this->$cariID); echo '</pre>';*/
+# untuk carian msic/produk/kod ngdbbp
+echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
+echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
+
+
+//$carian = '| jumlah : ' . $this->cariID;
+//$carian = '| carian : ' . $this->carian[0];
+//include 'papar_jadual_berulang.php';
+//$pilihJadual = 'jadual_operasi';
+//$pilihJadual = 'jadual_am';
+//include 'papar_jadual_berulang_mudah.php';
