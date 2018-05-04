02b69592e95d6ac5df94e204eb3da0d872131f7a
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-04 12:01:02 +0800

    untuk fail standard bagi sebarang carian

--- Aplikasi/Fail/Papar/cari/index.php
+++ Aplikasi/Fail/Papar/cari/index.php
@@ -1,16 +1,17 @@
 <?php
+# pilih paparan ke bawah atau melintang
+//$pilihJadual = 'jadual_operasi';
+$pilihJadual = 'jadual_am';
+
 # untuk carian syarikat sahaja
 /*echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
 echo '<pre>$carian='; print_r($this->carian[0]); echo '</pre>';
 echo '<pre>$cariID='; print_r($this->$cariID); echo '</pre>';*/
+//include 'papar_jadual_berulang_mudah.php';
+
 # untuk carian msic/produk/kod ngdbbp
 echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
-echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
+//echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 
-
-//$carian = '| jumlah : ' . $this->cariID;
-//$carian = '| carian : ' . $this->carian[0];
-//include 'papar_jadual_berulang.php';
-//$pilihJadual = 'jadual_operasi';
-//$pilihJadual = 'jadual_am';
-//include 'papar_jadual_berulang_mudah.php';
+# jenis template
+include 'template_' . $this->template . '.php';
