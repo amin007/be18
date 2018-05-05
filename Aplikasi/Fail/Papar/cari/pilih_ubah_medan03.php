353afb6dd3baeff676fccc6df43ccc07a26bec1a
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-05 22:27:49 +0800

    1. tukar HTML_input kepada Html_TD_Kawalan 2. ganti medanCarian(), komen medanTajuk() 3. dalam medanHantar, tukar $myTable kepada $_jadual

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan03.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan03.php
@@ -1,25 +1,28 @@
 <?php
-$html = new \Aplikasi\Kitab\Html_Input;
+//$html = new \Aplikasi\Kitab\Html_Input;
+$html = new Aplikasi\Kitab\Html_TD_Kawalan;
 $aksi = URL . 'kawalan/ubahSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
-$class2 = 'col-sm-6 '; # untuk $data ?>
+$class2 = 'col-sm-6 '; # untuk $data
+$html->medanCarian(
+	array($myTable, $this->senarai, $this->cariID, $this->_jadual)
+);?>
 <form method="POST" action="<?php echo $aksi ?>"
 class="form-horizontal"><!-- class="form-inline" --><?php
-$html->medanTajuk($myTable, $class1);
+//$html->medanTajuk($myTable, $class1);
 for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
 		?><div class="form-group"><?php echo "\n\t";
-		?><label for="inputTajuk" class="col-sm-1 control-label"><?php echo $key
+		?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
 		?></label><?php //echo "\n\t"; 
-		/*?><div class="<?php echo $class2 ?>"><?php*/
-		$paparData = $html->updateInput($class2, $myTable, $kira, $key, $data);
+		?><div class="<?php echo $class2 ?>"><?php
+		$paparData = $html->ubahInput($this->_cariIndustri, $myTable, $kira, $key, $data);
 		echo $paparData . "\n";
-		/*?></div><!-- / class="<?php echo $class ?>" --><?php*/
+		?></div><!-- / class="<?php echo $class ?>" --><?php
 		?></div><!-- / class="form-group" --><?php
 		echo "\n";
 	}## --------------------------------------------------------------------------
-}$html->medanHantar($myTable, $class1); 
+}$html->medanHantar($this->_jadual, $class1);
 /*<!-- / class="container" -->*/ ?>
 </form><!-- / class="form-horizontal" -->
-
