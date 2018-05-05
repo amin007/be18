8000d420313f04b33d5474a24390c7cfe00d8aa0
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-05 18:12:13 +0800

    komen sementara untuk $carian

--- Aplikasi/Fail/Papar/cari/b_ubah.php
+++ Aplikasi/Fail/Papar/cari/b_ubah.php
@@ -5,16 +5,15 @@
 //$pilihJadual = 'ubah_medan01';
 $pilihJadual = 'ubah_medan02';
 
-# untuk carian syarikat sahaja
+# untuk kod lama
 /*echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
 echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
 echo '<pre>$cariID='; print_r($this->$cariID); echo '</pre>';//*/
 //include 'papar_jadual_berulang_mudah.php';
 
-# untuk carian msic/produk/kod ngdbbp
+# untuk kod baru
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
-echo '<pre>$dataID='; print_r($this->dataID); echo '</pre>';
-echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
+//echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 
 # jenis template
 include 'template_' . $this->template . '.php';
