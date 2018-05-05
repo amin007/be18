e6a0f20c1a4ef46759a4d0dfec9ebc11c8bd80d2
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-05 16:21:43 +0800

    tambah koding baru

--- Aplikasi/Fail/Papar/template/A0/diatas.php
+++ Aplikasi/Fail/Papar/template/A0/diatas.php
@@ -1,8 +1,10 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
 <title><?php
 /*
  style="background: url('<?php echo GAMBAR ?>') no-repeat center center fixed;background-size: cover;"
