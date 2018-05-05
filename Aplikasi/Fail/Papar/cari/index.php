d7fd0e34b1bc096e849dcb4b0feddf68970ee098
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-05 20:18:29 +0800

    cuba untuk seragamkan paparan untuk senarai data dan borang ubah data

--- Aplikasi/Fail/Papar/cari/index.php
+++ Aplikasi/Fail/Papar/cari/index.php
@@ -2,8 +2,9 @@
 # pilih paparan ke bawah atau melintang
 //$pilihJadual = 'jadual_operasi';
 $pilihJadual = 'jadual_am';
-//$pilihJadual = 'ubah_medan01';
-//$pilihJadual = 'ubah_medan02';
+//$pilihJadual = 'ubah_medan01'; # borang biodata berasaskan table
+//$pilihJadual = 'ubah_medan02'; # borang biodata berasaskan bootstrap
+//$pilihJadual = 'ubah_medan03'; # borang kawalan berasaskan bootstrap
 
 # untuk kod lama
 /*echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
@@ -14,6 +15,7 @@ echo '<pre>$cariID='; print_r($this->$cariID); echo '</pre>';*/
 # untuk kod baru
 echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
+//echo '<pre>$_cariIndustri='; print_r($this->_cariIndustri); echo '</pre>';
 
 # jenis template
 include 'template_' . $this->template . '.php';
