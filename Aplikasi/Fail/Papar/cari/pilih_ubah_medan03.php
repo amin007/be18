dc9ee8b77599de1b0bd93d9df3cab6503ca96320
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-05 19:30:59 +0800

    tambah fail khas untuk kawalan

---
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan03.php
@@ -0,0 +1,26 @@
+<?php
+$html = new \Aplikasi\Kitab\Html_Input;
+$aksi = URL . 'kawalan/ubahSimpan/' . $this->carian[0];
+$class1 = 'col-sm-7'; # untuk tajuk dan hantar
+$class2 = 'col-sm-6 '; # untuk $data ?>
+<form method="POST" action="<?php echo $aksi ?>"
+class="form-horizontal"><!-- class="form-inline" --><?php
+$html->medanTajuk($myTable, $class1); 
+for ($kira=0; $kira < count($row); $kira++)
+{	foreach ( $row[$kira] as $key=>$data )
+	{## papar data $row ------------------------------------------------
+		?><div class="form-group"><?php echo "\n\t";
+		?><label for="inputTajuk" class="col-sm-1 control-label"><?php echo $key 
+		?></label><?php //echo "\n\t"; 
+		/*?><div class="<?php echo $class2 ?>"><?php*/
+		$paparData = $html->updateInput($class2, $myTable, $kira, $key, $data);
+		echo $paparData . "\n";
+		/*?></div><!-- / class="<?php echo $class ?>" --><?php*/
+		?></div><!-- / class="form-group" --><?php
+		echo "\n";
+	}#-----------------------------------------------------------------
+}#-----------------------------------------------------------------
+$html->medanHantar($myTable, $class1); 
+/*<!-- / class="container" -->*/ ?>
+</form><!-- / class="form-horizontal" -->
+
