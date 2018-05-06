f5726b974b74f4d02210449122e577cd2be4aee8
Amin007 Ledang <helmi007@yahoo.com>
2018-05-06 11:12:55 +0800

    buang komen

--- Aplikasi/Kelas/Utama/Kawal/cari.php
+++ Aplikasi/Kelas/Utama/Kawal/cari.php
@@ -264,8 +264,8 @@ class Cari extends \Aplikasi\Kitab\Kawal
 				: '*';
 			$carian = $this->tanya->bentukCarian($_POST['jika'], $myTable);
 			$this->papar->senarai[$myTable] = $this->tanya->
-				cariSql($namaPanjang, $medan, $carian, $susun);
-				//cariSemuaData($namaPanjang, $medan, $carian, $susun);
+				//cariSql($namaPanjang, $medan, $carian, $susun);
+				cariSemuaData($namaPanjang, $medan, $carian, $susun);
 		}# tamat ulang table
 
 		# papar jadual kod unit
