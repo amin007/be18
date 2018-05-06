f6e9c0a232ce3be521d2c6128d124114b7c8f056
Amin007 Ledang <helmi007@yahoo.com>
2018-05-06 11:12:14 +0800

    papar hasil carian

--- Aplikasi/Fail/Papar/cari/b_ubah.php
+++ Aplikasi/Fail/Papar/cari/b_ubah.php
@@ -17,5 +17,14 @@ echo '<pre>$cariID='; print_r($this->$cariID); echo '</pre>';//*/
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 //echo '<pre>$_cariIndustri='; print_r($this->_cariIndustri); echo '</pre>';
 
+# papar hasil carian
+$cari1 = '&nbsp;|&nbsp;'; $cari2 = '';
+foreach ($this->carian as $kunci => $nilai)
+	$cari1 .= ( count($nilai)==0 ) ? $nilai : $nilai . ' | ';
+foreach ($this->senarai as $kunci2 => $nilai2)
+	$cari2 .= ( count($nilai2)==0 ) ? $kunci2 . " = Kosong<br>\r"
+	: $kunci2 . ' = ' . count($nilai2) . "<br>\r";
+echo "Anda mencari = $cari1\r<br>$cari2\r<hr>";
+
 # jenis template
 include 'template_' . $this->template . '.php';
