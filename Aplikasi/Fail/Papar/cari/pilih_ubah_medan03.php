561df6ed97d551913988cf137c9d2c2664592270
Amin007 Ledang <helmi007@yahoo.com>
2018-05-06 15:26:43 +0800

    1. guna class Aplikasi\Kitab\Borang02_Ubah 2. tambah $this->_method untuk $html->medanCarian, $aksi 3. buang $html->medanTajuk() dan <!-- class="form-inline" -->

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan03.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan03.php
@@ -1,28 +1,25 @@
 <?php
-//$html = new \Aplikasi\Kitab\Html_Input;
-$html = new Aplikasi\Kitab\Html_TD_Kawalan;
-$aksi = URL . 'kawalan/ubahSimpan/' . $this->carian[0];
+$html = new Aplikasi\Kitab\Borang02_Ubah;
+$aksi = URL . $this->_method . '/ubahSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6 '; # untuk $data
 $html->medanCarian(
-	array($myTable, $this->senarai, $this->cariID, $this->_jadual)
+	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
 );?>
 <form method="POST" action="<?php echo $aksi ?>"
-class="form-horizontal"><!-- class="form-inline" --><?php
-//$html->medanTajuk($myTable, $class1);
+class="form-horizontal"><?php echo "\n";
 for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
 		?><div class="form-group"><?php echo "\n\t";
 		?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
-		?></label><?php //echo "\n\t"; 
+		?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
 		$paparData = $html->ubahInput($this->_cariIndustri, $this->_jadual,
 			$kira, $key, $data);
-		echo $paparData . "\n";
-		?></div><!-- / class="<?php echo $class2 ?>" --><?php
-		?></div><!-- / class="form-group" --><?php
-		echo "\n";
+		echo $paparData . "\n\t";
+		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
+		?></div><!-- / class="form-group" --><?php echo "\n";
 	}## --------------------------------------------------------------------------
 }$html->medanHantar($this->_jadual, $class1);
 /*<!-- / class="container" -->*/ ?>
