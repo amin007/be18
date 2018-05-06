7eaac168536ed91b256900cbd6b4b4ef924338c9
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-05-07 01:07:34 +0800

    buang kod lama yang tidak diperlukan lagi

--- Aplikasi/Fail/Papar/cari/index.php
+++ Aplikasi/Fail/Papar/cari/index.php
@@ -4,13 +4,6 @@
 $pilihJadual = 'jadual_am';
 //$pilihJadual = 'ubah_medan01'; # borang biodata berasaskan table
 //$pilihJadual = 'ubah_medan02'; # borang biodata berasaskan bootstrap
-//$pilihJadual = 'ubah_medan03'; # borang kawalan berasaskan bootstrap
-
-# untuk kod lama
-/*echo '<pre>$primaryKey='; print_r($this->primaryKey); echo '</pre>';
-echo '<pre>$carian='; print_r($this->carian[0]); echo '</pre>';
-echo '<pre>$cariID='; print_r($this->$cariID); echo '</pre>';*/
-//include 'papar_jadual_berulang_mudah.php';
 
 # untuk kod baru
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
