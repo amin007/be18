fe3c039911fe306389a159663efe828676c163ba
Amin Ledang @ Toshiba <helmi007@yahoo.com>
2018-05-20 12:31:53 +0800

    buang if

--- Aplikasi/Fail/Papar/cari/template_atas.php
+++ Aplikasi/Fail/Papar/cari/template_atas.php
@@ -1,21 +1,16 @@
 <?php
-if ($this->carian=='[id:0]')
-{
-	echo 'data kosong<br>';
-}
-else
-{ // $this->carian=='newss' - mula
-	$carian = (!isset($this->cariID)) ? null : $this->cariID;
-	$cariBatch = (!isset($this->cariBatch)) ? null : $this->cariBatch;
-	$mencari = URL . 'batch/ubahBatchAwal/' . $cariBatch;
-	$cetakF3 = URL . 'laporan/cetakf3/' . $cariBatch;
-	$cetakF3semua = URL . 'laporan/cetakf3semua/' . $cariBatch . "/500";
-	$cetakF3mfg = URL . 'laporan/cetakf3mfg/' . $cariBatch . "/500";
-	$cetakF3ppt = URL . 'laporan/cetakf3ppt/' . $cariBatch . "/500"; ?>
+$carian = (!isset($this->cariID)) ? null : $this->cariID;
+$cariBatch = (!isset($this->cariBatch)) ? null : $this->cariBatch;
+$mencari = URL . 'batch/ubahBatchAwal/' . $cariBatch;
+$cetakF3 = URL . 'laporan/cetakf3/' . $cariBatch;
+$cetakF3semua = URL . 'laporan/cetakf3semua/' . $cariBatch . "/500";
+$cetakF3mfg = URL . 'laporan/cetakf3mfg/' . $cariBatch . "/500";
+$cetakF3ppt = URL . 'laporan/cetakf3ppt/' . $cariBatch . "/500"; ?>
 <h3><a target="_blank" href="<?=$cetakF3semua?>">Cetak F3 Semua</a>
 |<a target="_blank" href="<?=$cetakF3mfg?>">F3 MFG</a>
 |<a target="_blank" href="<?=$cetakF3ppt?>">F3 PPT</a>
-| Ubah BatchAwal : <?=$cariBatch?><small>Nota: <?=$this->error?></small></h3>
+| Ubah BatchAwal : <?=$cariBatch?>
+| <small>Nota: <?=$this->error?></small></h3>
 <div align="center"><form method="GET" action="<?=$mencari?>" class="form-inline" autocomplete="off">
 <div class="form-group"><div class="input-group">
 	<input type="text" name="cari" class="form-control" autofocus
@@ -28,4 +23,3 @@ else
 	<div class="suggestionList" id="autoSuggestionsList">&nbsp;</div>
 </div>
 </form></div><br>
-<?php } // $this->carian=='newss'' - tamat ?>
