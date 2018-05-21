a256690b28391cbcae5566cc465a7c8c683fad16
Amin007 Ledang <helmi007@yahoo.com>
2018-05-21 16:46:53 +0800

    tambah jadual_biodata4

--- Aplikasi/Fungsi/Fungsi.php
+++ Aplikasi/Fungsi/Fungsi.php
@@ -89,6 +89,8 @@ function dpt_senarai($pilih)
 		$jadual = unserialize(JADUAL_BIODATA2);
 	elseif ($pilih == 'jadual_biodata3') :
 		$jadual = unserialize(JADUAL_BIODATA3);
+	elseif ($pilih == 'jadual_biodata4') :
+		$jadual = unserialize(JADUAL_BIODATA4);
 	elseif ($pilih == 'staf_prosesan') :
 		$jadual = unserialize(PROSESAN);
 	elseif ($pilih == 'staf_biasa') :
