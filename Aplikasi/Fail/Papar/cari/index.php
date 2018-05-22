2f3d29b2e66bf785220c42e21adf6914d7ac2d54
Amin007 Ledang <helmi007@yahoo.com>
2018-05-22 15:48:10 +0800

    sesuaikan ikut template

--- Aplikasi/Fail/Papar/cari/index.php
+++ Aplikasi/Fail/Papar/cari/index.php
@@ -18,7 +18,7 @@ foreach ($this->senarai as $kunci2 => $nilai2)
 	: $kunci2 . ' = ' . count($nilai2) . "<br>\r";
 echo "Anda mencari = $cari1\r<br>$cari2\r<hr>";//*/
 
-if(!isset($this->cariID))
-	echo '<h1>data kosong daa</h1>';
-else # jenis template
+//if(!isset($this->cariID))
+//	echo '<h1>data kosong daa</h1>';
+//else # jenis template
 	include 'template_' . $this->template . '.php';
