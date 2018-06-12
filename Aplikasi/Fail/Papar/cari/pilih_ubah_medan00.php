a62a322503c93b2c0a640a63b94538ab89117515
Amin007 Ledang <helmi007@yahoo.com>
2018-06-12 12:42:27 +0800

    tambah fungsi tambahDropInput(), buang _jadual

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
@@ -4,7 +4,7 @@ $aksi = URL . $this->_method . '/tambahSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6 '; # untuk $data
 $html->medanCarian(
-	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
+	array($this->_method, $myTable, $this->senarai, $this->cariID, null)
 );?>
 
 	<table class="excel">
@@ -15,7 +15,8 @@ $html->medanCarian(
 		foreach ( $row[$kira] as $key=>$data )
 		{
 			?><tr><td><?php echo $key ?></td><?php
-			$paparData = $html->ubahInput($this->_cariIndustri, $this->_jadual,
+			?><td><?php echo $data ?></td><?php
+			$paparData = $html->tambahDropInput($this->_paparMedan, $myTable,
 			$kira, $key, $data);
 			?><td><?php echo $paparData . "\n\t"
 			?></td></tr><?php
