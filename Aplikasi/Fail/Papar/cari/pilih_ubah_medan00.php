be2a8cc56baa7b0abaa43ee07cf3e6111ab43d88
Amin007 Ledang <helmi007@yahoo.com>
2018-06-12 15:16:05 +0800

    buang istihar class

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
@@ -1,12 +1,3 @@
-<?php
-$html = new Aplikasi\Kitab\Borang02_Ubah;
-$aksi = URL . $this->_method . '/tambahSimpan/' . $this->carian[0];
-$class1 = 'col-sm-7'; # untuk tajuk dan hantar
-$class2 = 'col-sm-6 '; # untuk $data
-$html->medanCarian(
-	array($this->_method, $myTable, $this->senarai, $this->cariID, null)
-);?>
-
 	<table class="excel">
 	<?php
 	for ($kira=0; $kira < count($row); $kira++)
