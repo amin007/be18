e44f761657cfda369d2a2ecd5f122c79ac24cc92
Amin007 Ledang <helmi007@yahoo.com>
2018-07-02 13:02:14 +0800

    tambah template A4.1.1

---
+++ Aplikasi/Fail/Papar/template/A4.1.1/menu_atas.php
@@ -0,0 +1,40 @@
+<!-- menu_atas.php A0 --><?php 
+$sesi = \Aplikasi\Kitab\Sesi::init();
+//echo '<pre>MENU_ATAS - $_SESSION:', print_r($_SESSION, 1) . '</pre><br>';
+# set pembolehubah
+$pengguna = \Aplikasi\Kitab\Sesi::get('namaPendek');
+$level = \Aplikasi\Kitab\Sesi::get('levelPengguna');
+
+$senaraiPengguna = array('pentadbir','biasa');
+$senaraiPentadbir = array('pentadbir','biasa');
+if (in_array($level, $senaraiPentadbir)) 
+	$paras = '' . $level;
+elseif (in_array($level, $senaraiPengguna))
+	$paras = '' . $level;
+else 
+	$paras = null; # untuk pelawat sahaja
+
+$iconFA['home2'] = '<i class="fa fa-home fa-2x" aria-hidden="true"></i>';
+$iconFA['video'] = '<i class="fa fa-video-camera" aria-hidden="true"></i>';
+
+echo "\n\n"; 
+//if ($paras == null): else: ?>
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+	<a class="navbar-brand" href="<?php echo URL ?>">
+		<?php echo $iconFA['video'] . Tajuk_Muka_Surat . ':' . $paras ?></a>
+	<a class="navbar-brand" href="<?php echo URL ?>ruangtamu/logout">
+		<i class="fa fa-times fa-2x" aria-hidden="true"></i>Keluar</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<!-- form class="form-inline w-90" -->
+	<input class="form-control w-70" type="text" placeholder="Search" aria-label="Search">
+<!-- /form></ class="form-inline" -->
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<?php require 'menubar_atas.php'; ?>
+</nav>
+<?php
+// endif;
