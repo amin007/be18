bd57595047732f45395cf9f523ea92c9144c058b
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-08-13 15:51:34 +0800

    untuk contoh fail-fail

---
+++ Aplikasi/Fail/Papar/borang/pilih_jadual_am2.php
@@ -0,0 +1,30 @@
+	<table class="excel">
+	<thead><tr>
+		<th>nama medan</th>
+		<th>data</th>
+		<th>peratusan</th>
+		<th>anggar</th>
+		<th>ubahsuai</th>
+	</tr></thead>
+	<?php
+	for ($kira=0; $kira < count($row); $kira++)
+	{## papar data $row ------------------------------------------------
+	?><tbody><?php
+		foreach ( $row[$kira] as $key=>$data )
+		{
+			echo "\n\t";
+			?><tr><td align="right"><?php echo $key ?></td><?php
+			//$paparData = $html->tambahDropInput($this->_paparMedan, $this->_j2,
+			//$myTable, $kira, $key, $data);
+			/*?><td><?php echo $paparData . "\n\t" ?></td><?php*/
+			?><td><?php echo $data ?></td><?php
+			?><td><?php echo formula01($key,$data) ?></td><?php
+			?><td><?php echo $data ?></td><?php
+			?><td><?php echo $data ?></td><?php
+			?></tr><?php 
+		}
+		?></tbody>
+	<?php
+	}#-----------------------------------------------------------------
+	?></table>
+
