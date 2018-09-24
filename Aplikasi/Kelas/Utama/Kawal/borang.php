f098176a97ef0bd8f4c2ded7b26ec87f8bae6a90
Amin007 Ledang <helmi007@yahoo.com>
2018-09-24 10:01:35 +0800

    tutup komen

--- Aplikasi/Kelas/Utama/Kawal/borang.php
+++ Aplikasi/Kelas/Utama/Kawal/borang.php
@@ -305,10 +305,10 @@ class Borang extends \Aplikasi\Kitab\Kawal
 		$this->panggilDB('semuaBE',$db . 'be2016_v2',$idBorang);
 		$this->panggilDBKhas01('hasilBE',$db . 'be2016_hasil_servis',$idBorang);
 		$this->panggilDBKhas01('belanjaBE',$db . 'be2016_belanja_servis',$idBorang);
-		$this->panggilDB('stafBE',$db . 'be2016_staf_servis02',$idBorang);
-		$this->debugKandunganPaparan();
+		//$this->panggilDB('stafBE',$db . 'be2016_staf_servis02',$idBorang);
+		//$this->debugKandunganPaparan();
 
-		/*# Pergi papar kandungan
+		# Pergi papar kandungan
 		$this->_folder = 'borang';
 		//echo '<br>$this->_folder = ' . $this->_folder . '<hr>';
 		$fail = array('index','index2','b_ubah','b_ubah_kawalan');
