48a9acb140caccde9f579d718d916b48117ca335
Amin007 Ledang <helmi007@yahoo.com>
2018-09-24 10:17:23 +0800

    pindah ke paparInput01()

--- Aplikasi/Fail/Papar/borang/pilih_jadual_am2.php
+++ Aplikasi/Fail/Papar/borang/pilih_jadual_am2.php
@@ -2,6 +2,7 @@
 	<h3><?php echo $tajukjadual ?></h3>
 	<thead><tr>
 		<th>nama medan</th>
+		<th>keterangan</th>
 		<th>data</th>
 		<th>peratusan</th>
 		<th>anggar</th>
@@ -13,19 +14,13 @@
 	?><tbody><?php
 		foreach ( $row[$kira] as $key=>$data )
 		{
-			echo ($data == '0') ? '' : "\n\t";
-			?><tr><td align="right"><?php echo $key ?></td><?php
-			//$paparData = $html->tambahDropInput($this->_paparMedan, $this->_j2,
-			//$myTable, $kira, $key, $data);
-			/*?><td><?php echo $paparData . "\n\t" ?></td><?php*/
-			?><td><?php echo $data ?></td><?php
-			?><td><?php echo formula01($key,$data) ?></td><?php
-			?><td><?php echo $data ?></td><?php
-			?><td><?php echo $data ?></td><?php
-			?></tr><?php 
+			if($data == '0'):
+				echo '<tr></tr>';
+			else:
+				paparInput01($key,$data);
+			endif;
 		}
 		?></tbody>
 	<?php
 	}#-----------------------------------------------------------------
 	?></table>
-
