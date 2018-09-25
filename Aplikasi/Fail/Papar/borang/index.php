94e987490b3dda485db3e074cee4a40b86d9626e
Amin007 Ledang <helmi007@yahoo.com>
2018-09-25 19:56:11 +0800

    buang fungsi dan 'template_'

--- Aplikasi/Fail/Papar/borang/index.php
+++ Aplikasi/Fail/Papar/borang/index.php
@@ -16,21 +16,5 @@ $pilihJadual = 'jadual_am2'; # ubah suai data
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 
-include 'template_' . $this->template . '.php';
+include $this->template . '.php';
 include 'atas/dibawah.php';
-
-function formula01($key,$data)
-{
-	$buang01 = array('newss','nama','batch','form','kp','msic2008');
-	if (in_array($key,$buang01)):
-		$papar = null;
-	elseif($data == '0'):
-		$papar = null;
-	else:
-		$jumlah = '1969435';
-		$kiraan = $data / $jumlah;
-		$papar = kiraPerpuluhan($kiraan, $perpuluhan = 2) . '%';
-	endif;
-
-	return $papar;
-}
