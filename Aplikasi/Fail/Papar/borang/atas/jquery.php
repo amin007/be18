e47c70a77556adf8cc9577826c1b01468d8200a4
Amin007 Ledang <helmi007@yahoo.com>
2018-09-25 17:09:24 +0800

    tambah cariapa dalam tatasusunan $classKhas

--- Aplikasi/Fail/Papar/borang/atas/jquery.php
+++ Aplikasi/Fail/Papar/borang/atas/jquery.php
@@ -15,7 +15,7 @@ if (isset($this->js))
 ?><?php 
 $dataURL = dpt_url();
 //echo '<pre>'; echo '<br>$dataURL:<br>'; print_r($dataURL); echo '</pre>';
-$classKhas = array('ubah','hadirbulan','awal','batch','senarai');
+$classKhas = array('ubah','hadirbulan','awal','batch','senarai','cariapa');
 if ( isset($dataURL[1]) && ( in_array($dataURL[1],$classKhas) )) :
 echo "\n\n"; ?>
 <script type="text/javascript">
