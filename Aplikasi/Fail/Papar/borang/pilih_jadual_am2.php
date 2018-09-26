2710c71b29dfac1a822d5dde644a58721f401b6b
Amin007 Ledang <helmi007@yahoo.com>
2018-09-26 19:38:55 +0800

    tambah medan lagi

--- Aplikasi/Fail/Papar/borang/pilih_jadual_am2.php
+++ Aplikasi/Fail/Papar/borang/pilih_jadual_am2.php
@@ -1,7 +1,11 @@
 	<table><tr><td>
 	<table class="excel">
 	<h3><?php echo $tajukjadual ?></h3>
-	<thead><tr><th>keterangan</th><th>nama medan</th><th>data</th><th>peratusan</th><th>anggar</th><th>ubahsuai</th></tr></thead>
+	<thead><tr><th>keterangan</th><th>nama medan</th><th>data</th><th>peratusan</th>
+		<th>anggar puluh</th>
+		<th>anggar fix</th>
+		<th>ubahsuai</th>
+	</tr></thead>
 	<?php
 	for ($kira=0; $kira < count($row); $kira++)
 	{## papar data $row -----------------------------------------------------
