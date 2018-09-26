78730e70dbe7bc3a0761fe0fadaaf380e1cc95b7
Amin007 Ledang <helmi007@yahoo.com>
2018-09-26 09:44:22 +0800

    ubah paparan output html

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
@@ -8,9 +8,9 @@
 			?><tr><td align="right"><?php echo $key ?></td><?php
 			$paparData = $html->tambahDropInput($this->_paparMedan, $this->_j2,
 			$myTable, $kira, $key, $data);
-			?><td><?php echo $paparData . "\n\t" ?></td><?php
+			?><td><?php echo $paparData ?></td><?php
 			?><td><?php echo $data ?></td><?php
-			?></tr><?php
+			?></tr><?php echo "\n\t";
 		}
 		?></tbody>
 	<?php
