3ced65f236910722c024267b3674fee6add24a6d
Amin007 Ledang <helmi007@yahoo.com>
2018-09-26 18:09:49 +0800

    1. tambah tatasusunan $buang. 2. alih kedudukan fungsi ikut keutamaan

--- Aplikasi/Fail/Papar/borang/index2.php
+++ Aplikasi/Fail/Papar/borang/index2.php
@@ -5,7 +5,8 @@ include 'atas/diatas.php';
 $class1 = 'col-sm-6'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6'; # untuk $data
 $aksi = null;
-
+$buang01 = array('newss','nama','batch','form','kp','msic2008',
+	'KodBanci','NoSiri','F0002','F0014','F0015');
 # include fail
 include $this->template . '.php';
 include $this->template2 . '.php';
@@ -14,7 +15,7 @@ include $this->template2 . '.php';
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 //echo '<pre>$medan='; print_r($this->medan); echo '</pre>';
-echo '<pre>$_5p='; print_r($this->_5p); echo '</pre>';
+//echo '<pre>$_5p='; print_r($this->_5p); echo '</pre>';
 /*$ulang = array('kodbanci','nosiri','F0002','F0014','F0015',
 	'F2001','F2089','F2099');
 foreach ($ulang as $key):
@@ -24,6 +25,34 @@ endforeach;//*/
 //include 'soalan_anggaran.php';
 include 'atas/dibawah.php';
 
+#--------------------------------------------------------------------------------------------
+function keteranganMedan($key,$banyakMedan)
+{
+	# cari keterangan medan yang lain
+	$namaMedan = null;
+	foreach ($banyakMedan as $p0 => $p1):
+	foreach ($p1 as $n0 => $data):
+		if($key == $data)
+			$namaMedan = $banyakMedan[$p0]['keterangan'];
+	endforeach;endforeach;
+
+	return $namaMedan;
+}
+#--------------------------------------------------------------------------------------------
+function paparInput01($myTable,$key,$data,$banyakMedan,$_5p)
+{
+	?><tr><?php
+	?><td align="right"><?php echo keteranganMedan($key,$banyakMedan) ?></td><?php
+	//$paparData = $html->tambahDropInput($this->_paparMedan, $this->_j2,
+	//$myTable, $kira, $key, $data);
+	/*?><td><?php echo $paparData . "\n\t" ?></td><?php*/
+	?><td><?php echo $key ?></td><?php
+	?><td><?php echo $data ?></td><?php
+	?><td><?php echo formula01($myTable,$key,$data,$_5p) ?></td><?php
+	?><td><?php echo $data ?></td><?php
+	?><td><?php echo $data ?></td><?php
+	?></tr><?php
+}
 #--------------------------------------------------------------------------------------------
 function formula00()
 {
@@ -56,34 +85,5 @@ function formula01($myTable,$key,$data,$_5p)
 	endif;
 
 	return $papar;
-}
-#--------------------------------------------------------------------------------------------
-function paparInput01($myTable,$key,$data,$banyakMedan,$_5p)
-{
-	?><tr><?php
-	?><td align="right"><?php echo keteranganMedan($key,$banyakMedan) ?></td><?php
-	//$paparData = $html->tambahDropInput($this->_paparMedan, $this->_j2,
-	//$myTable, $kira, $key, $data);
-	/*?><td><?php echo $paparData . "\n\t" ?></td><?php*/
-	?><td><?php echo $key ?></td><?php
-	?><td><?php echo $data ?></td><?php
-	?><td><?php echo formula01($myTable,$key,$data,$_5p) ?></td><?php
-	?><td><?php echo $data ?></td><?php
-	?><td><?php echo $data ?></td><?php
-	?></tr><?php
-}
-#--------------------------------------------------------------------------------------------
-
-function keteranganMedan($key,$banyakMedan)
-{
-	# cari keterangan medan yang lain
-	$namaMedan = null;
-	foreach ($banyakMedan as $p0 => $p1):
-	foreach ($p1 as $n0 => $data):
-		if($key == $data)
-			$namaMedan = $banyakMedan[$p0]['keterangan'];
-	endforeach;endforeach;
-
-	return $namaMedan;
 }
 #--------------------------------------------------------------------------------------------
