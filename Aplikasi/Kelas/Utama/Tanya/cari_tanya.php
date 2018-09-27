982f28a87dfa2d9bf15b71c8c26e918ab36ba544
Amin007 Ledang <helmi007@yahoo.com>
2018-09-27 17:30:57 +0800

    tambah nilai dalam tatasusunan

--- Aplikasi/Kelas/Utama/Tanya/cari_tanya.php
+++ Aplikasi/Kelas/Utama/Tanya/cari_tanya.php
@@ -164,7 +164,7 @@ class Cari_Tanya extends \Aplikasi\Kitab\Tanya
 		'kp_bst_q1_2017','kp_bst_q1_2018','kp_bst_q4_2017','kp_ejob_2018',
 		'kp_mdt_2017','kp_mdt_2018','kp_mm17','kp_mm18','kp_mm_2017','kp_pan17',
 		'kp_qss_2017','kp_qss_2018','kp_qss_2018_data',
-		'sample_kp411','sdsk_kp411');
+		'sample_kp411','sdsk_kp411','data_banci');
 		$medan = '*';
 		# cari id berasaskan newss/ssm/sidap/nama
 		//$id['nama'] = bersih(isset($_POST['cari']) ? $_POST['cari'] : null);
